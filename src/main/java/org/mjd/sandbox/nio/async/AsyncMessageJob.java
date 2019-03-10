39a2649050d1758dfc17c00d37260b27d1cb58ae
Mark Doyle <markjohndoyle@googlemail.com>
2019-03-10 14:47:01 +0100

    document AsyncMessageJob class

--- src/main/java/org/mjd/sandbox/nio/async/AsyncMessageJob.java
+++ src/main/java/org/mjd/sandbox/nio/async/AsyncMessageJob.java
@@ -7,12 +7,37 @@ import java.util.concurrent.Future;
 
 import org.mjd.sandbox.nio.message.Message;
 
+/**
+ * {@link AsyncMessageJob} is a data class that holds related information about an asynchronour message handling job,
+ * it's associated {@link SelectionKey} and the original {@link Message}
+ *
+ * @param <MsgType> the type of {@link Message} this job is for
+ */
 public final class AsyncMessageJob<MsgType> {
+	/**
+	 * {@link Future} holding the result of the message processing job.
+	 */
 	public final Future<Optional<ByteBuffer>> messageJob;
+
+	/**
+	 * The {@link SelectionKey} this asynchronous message processing job was started for
+	 */
 	public final SelectionKey key;
+
+	/**
+	 * The {@link Message} this asynchronous message processing job is processing/processed.
+	 */
 	public final Message<MsgType> message;
 
-	public AsyncMessageJob(SelectionKey key, Message<MsgType> message, Future<Optional<ByteBuffer>> asyncHandle) {
+	/**
+	 * Constructs a complete {@link AsyncMessageJob} for type MsgType.
+	 *
+	 * @param key
+	 * @param message
+	 * @param asyncHandle
+	 */
+	public AsyncMessageJob(final SelectionKey key, final Message<MsgType> message,
+						   final Future<Optional<ByteBuffer>> asyncHandle) {
 		this.messageJob = asyncHandle;
 		this.key = key;
 		this.message= message;
