316da8aca6bd46dceec319aea842f68e17e88381
Mark Doyle <markjohndoyle@googlemail.com>
2019-03-16 21:21:53 +0100

    remove unused imports

--- src/main/java/org/mjd/repro/Server.java
+++ src/main/java/org/mjd/repro/Server.java
@@ -1,7 +1,6 @@
 package org.mjd.repro;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
--- src/test/java/org/mjd/repro/ServerRpcSingleClientSubscribeIT.java
+++ src/test/java/org/mjd/repro/ServerRpcSingleClientSubscribeIT.java
@@ -3,7 +3,6 @@ package org.mjd.repro;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
