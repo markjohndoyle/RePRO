bbce99cc75091d2c48e3ac64b8217e7d01167c8b
Mark Doyle <markjohndoyle@googlemail.com>
2019-03-24 14:21:04 +0100

    remove unused code in IntMessage

--- src/main/java/org/mjd/repro/message/IntMessage.java
+++ src/main/java/org/mjd/repro/message/IntMessage.java
@@ -1,18 +1,14 @@
 package org.mjd.repro.message;
 
-import java.nio.ByteBuffer;
-
 import com.google.common.primitives.Ints;
 
 public final class IntMessage implements Message<Integer>
 {
     private final int value;
-    private final byte[] bytes;
 
-    public IntMessage(int value)
+    public IntMessage(final int value)
     {
         this.value = value;
-        bytes = ByteBuffer.allocate(size()).putInt(value).array();
     }
 
     /* (non-Javadoc)
@@ -33,16 +29,7 @@ public final class IntMessage implements Message<Integer>
         return Integer.BYTES;
     }
 
-    /* (non-Javadoc)
-     * @see org.mjd.repro.Message#asByteArray()
-     */
-//    @Override
-//    public byte[] asByteArray()
-//    {
-//        return bytes;
-//    }
-
-    public static Message<Integer> from(byte[] message)
+    public static Message<Integer> from(final byte[] message)
     {
         return new IntMessage(Ints.fromByteArray(message));
     }
