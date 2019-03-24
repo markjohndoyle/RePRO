834322c6f263ebf445057a9f8e919b9d2ae1eabc
Mark Doyle <markjohndoyle@googlemail.com>
2019-03-24 21:06:27 +0100

    document functional handler interface

--- src/main/java/org/mjd/repro/util/chain/Handler.java
+++ src/main/java/org/mjd/repro/util/chain/Handler.java
@@ -1,7 +1,21 @@
 package org.mjd.repro.util.chain;
 
+/**
+ * Functional interface that performs some action. The function will receive a parameter of type R.
+ *
+ * THis may seem rather abstract, this is because it's part of an implementation of the Chain of Responsibility
+ * pattern.
+ *
+ * @param <R> the type of requests this Handler handles.
+ */
+@FunctionalInterface
 public interface Handler<R> {
 
+	/**
+	 * Performs some action when given the request.
+	 *
+	 * @param request the request to handle
+	 */
 	void handle(R request);
 
 }
